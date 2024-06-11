379731721008b833b84fc902f216298f40c677e4
AnneKluytmans <annekluytmans1@gmail.com>
2024-06-11 16:25:06 +0200

    Added styling and created content on blog overview page

--- src/pages/blogOverviewPage/BlogOverview.jsx
+++ src/pages/blogOverviewPage/BlogOverview.jsx
@@ -1,8 +1,29 @@
+import data from '../../constants/data.json'
 import './BlogOverview.css';
 
 function BlogOverview() {
+    const amountOfBlogs = data.length
+    const blogs = data
+
     return (
-        <p>Blog overview</p>
+        <>
+            <header className="header--blog-overview outer-content-container">
+                <h1>Bekijk alle {amountOfBlogs} posts op het platform</h1>
+            </header>
+            <section className="outer-content-container">
+                <div className="blog-overview-container">
+                    {blogs.map((blog) => {
+                         return (
+                              <article key={blog.id} className="blog-item">
+                                  <h3>{blog.title}</h3>
+                                  <h4>({blog.author})</h4>
+                                  <p>{blog.comments} reacties - {blog.shares} keer gedeeld</p>
+                              </article>
+                       );
+                      })}
+                </div>
+            </section>
+        </>
     );
 }
 
--- src/pages/notFoundPage/NotFound.jsx
+++ src/pages/notFoundPage/NotFound.jsx
@@ -4,10 +4,10 @@ import './NotFound.css';
 function NotFound() {
     return (
       <>
-        <main className="not-found-page">
+        <section className="section--not-found inner-content-container">
             <h2>Oops... This page doesn't exist</h2>
-            <p>Take me back to the <Link to="/">home page</Link>.</p>
-        </main>
+            <p>Take me back to the <Link to="/" className="section--not-found__link">home page</Link>.</p>
+        </section>
       </>
     );
 }
