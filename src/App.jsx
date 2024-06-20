aec739bbfaa48521a70abfad12007c95b268d581
AnneKluytmans <annekluytmans1@gmail.com>
2024-06-20 12:56:35 +0200

    Finished assignment 1 of blogposts part 2: request exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Navigation from "./components/navigation/Navigation.jsx";
 import Home from "./pages/homePage/Home.jsx";
+import RequestExercises from "./pages/requestExercisesPage/RequestExercises.jsx";
 import NewPost from "./pages/newPostPage/NewPost.jsx";
 import BlogOverview from "./pages/blogOverviewPage/BlogOverview.jsx";
 import BlogDetail from "./pages/blogDetailPage/BlogDetail.jsx";
 import NotFound from "./pages/notFoundPage/NotFound.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import './App.css';
+
 
 function App() {
     return (
@@ -18,6 +20,7 @@ function App() {
                     <Route path="/blog-overview" element={<BlogOverview/>}/>
                     <Route path="/new-post" element={<NewPost/>}/>
                     <Route path="/blog-detail/:id" element={<BlogDetail/>}/>
+                    <Route path="/request-exercises" element={<RequestExercises/>}/>
                     <Route path="*" element={<NotFound />}/>
                 </Routes>
             </main>
